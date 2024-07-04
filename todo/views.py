ba2bf9841a1b1bbc5592863de0cd02006997b2f6
siko2525 <s1f102301820@iniad.org>
2024-07-04 13:42:03 +0900

    警告修正２

--- todo/views.py
+++ todo/views.py
@@ -15,7 +15,7 @@ def index(request):
     else:
         tasks = Task.objects.order_by('-posted_at')
 
-    context =  {
+    context = {
         'tasks': tasks
     }
     return render(request, 'todo/index.html', context)
