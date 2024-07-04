eb86c3b33c704a2d7a15cbe201601579599e235c
siko2525 <s1f102301820@iniad.org>
2024-07-04 13:13:29 +0900

    Initial commit

--- todo/tests.py
+++ todo/tests.py
@@ -36,6 +36,21 @@ class TaskModelTestCase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
 
+    def test_is_overdue_past(self):
+        due=timezone.make_aware(datetime(2024,6,30,23,59,59))
+        current=timezone.make_aware(datetime(2024,7,1,0,0,0))
+        task=Task(title='task1',due_at=due)
+        task.save()
+
+        self.assertTrue(task.is_overdue(current))
+
+    def test_is_overdue_none(self):
+        current=timezone.make_aware(datetime(2024,7,1,0,0,0))
+        task=Task(title='task2')
+        task.save()
+
+        self.assertFalse(task.is_overdue(current))
+
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
